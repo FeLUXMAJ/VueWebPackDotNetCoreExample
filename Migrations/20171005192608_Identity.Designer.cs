fdc1918c90ae83bb87c411232716f0f1876b2a93
Samuel Hilson <samrhilson@gmail.com>
2017-10-12 16:00:56 -0500

    Setup Identiy and create a registration page.

--- Migrations/20171005192608_Identity.Designer.cs
+++ Migrations/20171005192608_Identity.Designer.cs
@@ -11,9 +11,10 @@ using VueWebpackExample.Data;
 namespace VueWebpackExample.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171005192608_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
